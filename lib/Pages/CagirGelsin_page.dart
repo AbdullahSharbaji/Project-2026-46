1b6d42c634ee2242da385293bad43646d1198de7
AbdullahSharbaji <abdullahpc937@gmail.com>
2026-01-30 18:34:19 +0300

    Base UI Elements

---
+++ lib/Pages/CagirGelsin_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class CagirGelsin extends StatefulWidget {
+  const CagirGelsin({super.key});
+
+  @override
+  State<CagirGelsin> createState() => _CagirGelsinState();
+}
+
+class _CagirGelsinState extends State<CagirGelsin> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Cagir Gelsin')),
+      body: Center(child: Column(children: [
+            
+          ],
+        )),
+    );
+  }
+}
---
+++ lib/Pages/Hizmet_page.dart
@@ -0,0 +1,21 @@
+import 'package:flutter/material.dart';
+
+class Hizmet extends StatefulWidget {
+  const Hizmet({super.key});
+
+  @override
+  State<Hizmet> createState() => _HizmetState();
+}
+
+class _HizmetState extends State<Hizmet> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text('Hizmet Al')),
+      body: Center(child: Column(children: [
+            
+          ],
+        )),
+    );
+  }
+}
